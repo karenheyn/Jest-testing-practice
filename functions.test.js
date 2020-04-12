9497dad0748ac5ce41cdbfbe584e2e2a415fb412
karenheyn <karenheyn@gmail.com>
2020-04-11 23:15:38 -0400

    test 10, async data

--- functions.test.js
+++ functions.test.js
@@ -17,8 +17,40 @@ test("should be falsy", () => {
 });
 
 test("User should be Karen Heyn object", () => {
-  expect(functions.createUser()).toStrictEqual({
+  expect(functions.createUser()).toEqual({
     firstName: "Karen",
     lastName: "Heyn",
   });
+});
+
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/); //can use i to not be case sensitive
+});
+
+test("admin should be in usernames", () => {
+  usernames = ["bob", "jim", "admin"];
+  expect(usernames).toContain("admin");
+});
+
+//async data
+
+//promise
+test("user fetched name should be Leanne Graham", () => {
+  expect.assertions(1); //must have assertions and return
+  return functions
+    .fetchUser()
+    .then((data) => expect(data.name).toEqual("Leanne Graham"));
+});
+
+//async await
+test("user fetched name should be Leanne Graham", async () => {
+  expect.assertions(1); //must have assertions and return
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
 });
